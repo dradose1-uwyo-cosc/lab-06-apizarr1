83163928a9c7f0424dce3350f05d52a99da1935c
apizarr1 <apizarr1@uwyo.edu>
2024-10-20 19:48:14 -0600

    Update cosc1010-lab06.py
    final lab update, done in vs studio

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,12 +1,10 @@
-# Your Name Here
+# Angel Pizarro
 # UWYO COSC 1010
-# Submission Date
+# 10/20/24
 # Lab 06
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# Lab Section 18 
+# Sources, people worked with, help given to: w3schools.com
+
 
 
 random_string = """
@@ -98,3 +96,39 @@ print(f"The letter that occurred the most is {least_occurred}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+
+char_count = {}
+
+for char in random_string:
+    if char in char_count:
+        char_count[char] += 1
+    else:
+        char_count[char] = 1
+
+total_chars = len(random_string)
+sorted_keys = sorted(char_count.keys())
+
+print("*" * 75)
+for char in sorted_keys:
+    print(f"{char}: {char_count[char]}")
+
+
+most_occurred_char = sorted_keys[0]
+least_occured_char = sorted_keys[0]
+
+for char in sorted_keys:
+    if char_count[char] > char_count[most_occurred_char]:
+        most_occurred_char = char
+
+    if char_count[char] < char_count[least_occured_char]:
+        least_occured_char = char
+    
+print("*" * 75)
+print(f"The letter that occured the most is '{most_occurred_char}' with {char_count[most_occurred_char]} occurrences.")
+print("*" * 75)
+print(f"The letter that occured the least is '{least_occured_char}' with {char_count[least_occured_char]} occurrences.")
+print("*" * 75)
+
+for char in sorted_keys:
+    percentage = (char_count[char] / total_chars) * 100
+    print(f"{char}: {percentage:.2f}%")
